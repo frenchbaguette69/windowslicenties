93bf9e5fa13793eb8159383565151b531902a087
Arnob <arnob001.asa@gmail.com>
2024-11-15 20:04:59 +0600

    Fix build error

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Suspense } from "react";
 import AuthForm from "@/components/AuthForm";
 
 const SigninPage = () => {
 	return (
 		<div className='w-full mx-auto flex items-center justify-center py-6 lg:py-24'>
-			<AuthForm />
+			<Suspense fallback={<div>Loading...</div>}>
+				<AuthForm />
+			</Suspense>
 		</div>
 	);
 };
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,10 +1,12 @@
+import React, { Suspense } from "react";
 import AuthForm from "@/components/AuthForm";
-import React from "react";
 
 const SignupPage = () => {
 	return (
 		<div className='w-full mx-auto flex items-center justify-center py-6 lg:py-24'>
-			<AuthForm isRegister={true} />
+			<Suspense fallback={<div>Loading...</div>}>
+				<AuthForm isRegister={true} />
+			</Suspense>
 		</div>
 	);
 };
